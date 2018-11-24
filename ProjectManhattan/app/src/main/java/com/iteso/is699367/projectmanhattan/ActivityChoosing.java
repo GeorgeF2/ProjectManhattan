54a424f118eb9b57e8dc69c4ed697ef7267556c0
OverripeWings3 <sjacobust@gmail.com>
2018-11-24 13:42:33 -0600

    Changed Activity Choosing Help and view

--- ProjectManhattan/app/src/main/java/com/iteso/is699367/projectmanhattan/ActivityChoosing.java
+++ ProjectManhattan/app/src/main/java/com/iteso/is699367/projectmanhattan/ActivityChoosing.java
@@ -5,15 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ActivityChoosing extends AppCompatActivity {
 
     public static final String ROLE = "ROLE";
+    private ImageView help;
+    private TextView driveyHelp, rideyHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosing);
+
+
+        driveyHelp = findViewById(R.id.activity_choosing_drivey_help);
+        rideyHelp = findViewById(R.id.activity_choosing_ridey_help);
+
     }
 
     public void onRoleChosen (View v) {
@@ -31,4 +40,22 @@ public class ActivityChoosing extends AppCompatActivity {
         intent.putExtra(ROLE, role);
         startActivity(intent);
     }
+
+
+    public void appearHelp(View v) {
+
+
+        if (driveyHelp.getVisibility() == View.GONE && rideyHelp.getVisibility() == View.GONE) {
+            driveyHelp.setVisibility(View.VISIBLE);
+            rideyHelp.setVisibility(View.VISIBLE);
+        }
+        else {
+            driveyHelp.setVisibility(View.GONE);
+            rideyHelp.setVisibility(View.GONE);
+        }
+
+
+    }
+
+
 }

fa8ad1dde072f92fbbcc9d12aae988c5419ecb95
OverripeWings3 <sjacobust@gmail.com>
2018-12-02 17:15:28 -0600

    Added Activity for Ryders Gotten.

---
+++ ProjectManhattan/app/src/main/java/com/iteso/is699367/projectmanhattan/ActivityRydersGotten.java
@@ -0,0 +1,13 @@
+package com.iteso.is699367.projectmanhattan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityRydersGotten extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ryders_gotten);
+    }
+}
